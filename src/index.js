3f7d1737ee253c27c8da6de2153fd24426d929db
Bailey Benoit <Bailey.L.Benoit@gmail.com>
2024-06-29 12:14:53 -0700

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+console.log(5 + 5)
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  console.log(inputForm)
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+    
+    console.log(input.value)
+
+    fetch("http://localhost:3000/movies")
+        .then((response) => response.json())
+        .then((data) => {
+            const result = data.find(({id}) => id === input.value)
+            console.log(result);
+        });
+  });
+};
 
 document.addEventListener('DOMContentLoaded', init);
+
+console.log(5 + 5)
